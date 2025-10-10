5262b4982eec8d7cafdc8ad83a46aba8864df5e5
amabily04 <r.amabily@escola.pr.gov.br>
2025-10-10 07:57:59 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,13 @@ criaCartão(
     'O que é python?'
     'O python é uma linguagem de programação'
 )
+criaCartão(
+    'Geografia'
+    'Qual a capital da França?'
+    'A capital da França é Paris'
+)
+criaCartão(
+    'Programação'
+    'Oque éuma função?'
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
